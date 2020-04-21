2df6706fded307ced6fd365f5b189ffa5c10e464
leila <l.sedighi.2015@gmail.com>
2020-04-21 12:30:42 +0430

    init project

---
+++ app/src/main/java/com/example/soundmusicplayer/Music.kt
@@ -0,0 +1,9 @@
+package com.example.soundmusicplayer
+
+data class Music (
+        val id: String,
+        val title: String,
+        val album: String,
+        val singer: String,
+        val picPath: String
+)
---
+++ app/src/main/java/com/example/soundmusicplayer/MusicListFragment.kt
@@ -0,0 +1,20 @@
+package com.example.soundmusicplayer
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MusicListFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_music_list, container, false)
+    }
+
+}
